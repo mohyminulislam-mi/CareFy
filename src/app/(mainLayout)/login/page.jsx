2793c32d2af407c9af5673a4deb4a62922ef44bd
Mohyminul Islam <mohyminulmunna23@gmail.com>
2026-02-15 18:20:37 +0600

    update login form

--- src/app/(mainLayout)/login/page.jsx
+++ src/app/(mainLayout)/login/page.jsx
@@ -1,138 +1,58 @@
-"use client";
 import React from "react";
 
 const Login = () => {
-  const [state, setState] = React.useState("login");
-
-  const [formData, setFormData] = React.useState({
-    name: "",
-    email: "",
-    password: "",
-  });
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
-  };
   return (
     <div className="max-w-2xl mx-auto py-14">
-      <form
-        onSubmit={handleSubmit}
-        className="sm:w-[350px] w-full text-center border border-gray-300/60 rounded-2xl px-8 bg-white"
-      >
-        <h1 className="text-gray-900 text-3xl mt-10 font-medium">
-          {state === "login" ? "Login" : "Sign up"}
-        </h1>
-        <p className="text-gray-500 text-sm mt-2">Please sign in to continue</p>
-        {state !== "login" && (
-          <div className="flex items-center mt-6 w-full bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="#6B7280"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-user-round-icon lucide-user-round"
-            >
-              <circle cx="12" cy="8" r="5" />
-              <path d="M20 21a8 8 0 0 0-16 0" />
-            </svg>
-            <input
-              type="text"
-              name="name"
-              placeholder="Name"
-              className="border-none outline-none ring-0"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-          </div>
-        )}
-        <div className="flex items-center w-full mt-4 bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="14"
-            height="14"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="#6B7280"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-mail-icon lucide-mail"
-          >
-            <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
-            <rect x="2" y="4" width="20" height="16" rx="2" />
-          </svg>
+      <div className="bg-white text-gray-500 max-w-96 mx-4 md:p-6 p-4 text-left text-sm rounded-xl shadow-[0px_0px_10px_0px] shadow-black/10">
+        <h2 className="text-2xl font-semibold mb-6 text-center text-gray-800">
+          Welcome back
+        </h2>
+        <form>
           <input
+            id="email"
+            className="w-full bg-transparent border my-3 border-gray-500/30 outline-none rounded-full py-2.5 px-4"
             type="email"
-            name="email"
-            placeholder="Email id"
-            className="border-none outline-none ring-0"
-            value={formData.email}
-            onChange={handleChange}
+            placeholder="Enter your email"
             required
           />
-        </div>
-        <div className="flex items-center mt-4 w-full bg-white border border-gray-300/80 h-12 rounded-full overflow-hidden pl-6 gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="14"
-            height="14"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="#6B7280"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="lucide lucide-lock-icon lucide-lock"
-          >
-            <rect width="18" height="11" x="3" y="11" rx="2" ry="2" />
-            <path d="M7 11V7a5 5 0 0 1 10 0v4" />
-          </svg>
           <input
+            id="password"
+            className="w-full bg-transparent border mt-1 border-gray-500/30 outline-none rounded-full py-2.5 px-4"
             type="password"
-            name="password"
-            placeholder="Password"
-            className="border-none outline-none ring-0"
-            value={formData.password}
-            onChange={handleChange}
+            placeholder="Enter your password"
             required
           />
-        </div>
-        <div className="mt-4 text-left text-indigo-500">
-          <button className="text-sm" type="reset">
-            Forget password?
+          <div className="text-right py-4">
+            <a className="text-blue-600 underline" href="#">
+              Forgot Password
+            </a>
+          </div>
+          <button
+            type="submit"
+            className="w-full mb-3 bg-indigo-500 py-2.5 rounded-full text-white"
+          >
+            Log in
           </button>
-        </div>
-        <button
-          type="submit"
-          className="mt-2 w-full h-11 rounded-full text-white bg-indigo-500 hover:opacity-90 transition-opacity"
-        >
-          {state === "login" ? "Login" : "Sign up"}
-        </button>
-        <p
-          onClick={() =>
-            setState((prev) => (prev === "login" ? "register" : "login"))
-          }
-          className="text-gray-500 text-sm mt-3 mb-11"
-        >
-          {state === "login"
-            ? "Don't have an account?"
-            : "Already have an account?"}{" "}
-          <a href="#" className="text-indigo-500 hover:underline">
-            click here
+        </form>
+        <p className="text-center mt-4">
+          Don’t have an account?{" "}
+          <a href="#" className="text-blue-500 underline">
+            Signup
           </a>
         </p>
-      </form>
+
+        <button
+          type="button"
+          className="w-full flex items-center gap-2 justify-center my-3 bg-white border border-gray-500/30 py-2.5 rounded-full text-gray-800"
+        >
+          <img
+            className="h-4 w-4"
+            src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/login/googleFavicon.png"
+            alt="googleFavicon"
+          />
+          Log in with Apple
+        </button>
+      </div>
     </div>
   );
 };
